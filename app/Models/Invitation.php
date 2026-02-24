ad3387790c4e0503a0a85e72b21753086538bcd7
chouaib <choubbati@gmail.com>
2026-02-24 10:19:52 +0000

    ajouter model Invitation

--- app/Models/Invitation.php
+++ app/Models/Invitation.php
@@ -6,5 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 
 class Invitation extends Model
 {
-    //
+    protected $fillable = [
+        'colocation_id',
+        'email',
+        'token',
+        'status',
+        'expires_at',
+        'accepted_at',
+        'refused_at',
+    ];
+
+    protected $dates = [
+        'expires_at',
+        'accepted_at',
+        'refused_at',
+    ];
+
+    public function colocation(){
+        return $this->belongsTo(Colocation::class);
+    }
 }
