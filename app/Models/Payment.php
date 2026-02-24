42f406ba2ac52b7b0765567d088981613790833c
chouaib <choubbati@gmail.com>
2026-02-24 11:14:01 +0000

    ajouter model settlement et payment avec les relation

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -6,5 +6,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        'settlement_id',
+        'payer_id',
+        'receiver_id',
+        'amount',
+        'paid_at',
+    ];
+
+    protected $dates = ['paid_at'];
+
+    public function settlement()
+    {
+        return $this->belongsTo(Settlement::class);
+    }
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class, 'payer_id');
+    }
+
+    public function receiver()
+    {
+        return $this->belongsTo(User::class, 'receiver_id');
+    }
 }
