5a738b6b0385170b13a07e409a4a8c55b6508b8a
chouaib <choubbati@gmail.com>
2026-02-24 10:34:41 +0000

    ajouter model Expense

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -6,5 +6,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Expense extends Model
 {
-    //
+    protected $fillable = [
+        'title',
+        'amount',
+        'expense_date',
+        'payer_id',
+        'category_id',
+        'colocation_id',
+    ];
+
+    protected $casts= [
+        'expense_date'=> 'date',
+    ];
+
+    public function colocation()
+    {
+        return $this->belongsTo(Colocation::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+
+    }
+
+
+    public function payer()
+    {
+        return $this->belongsTo(User::class,'payer_id');
+    }
+
+
 }
