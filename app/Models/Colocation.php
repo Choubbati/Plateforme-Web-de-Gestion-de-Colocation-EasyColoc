51ce581e62d8876f657b57b3460f140c70539b61
chouaib <choubbati@gmail.com>
2026-02-24 10:04:42 +0000

    ajouter moder Colocation avec les relations

--- app/Models/Colocation.php
+++ app/Models/Colocation.php
@@ -6,5 +6,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class Colocation extends Model
 {
-    //
+    protected $fillable = [
+        'name','status','owner_id','cancelled_at'
+    ];
+
+    public function owner(){
+        return $this->belongsTo(User::class,'owner_id');
+    }
+
+    public function members(){
+        return $this->belongsToMany(User::class,'membership')
+            ->withPivot('role','joined_at','left_at')
+            ->withTimestamps();
+    }
+
+    public function memberships()
+    {
+        return $this->hasMany(Membership::class);
+    }
+
+    public function invitations()
+    {
+        return $this->hasMany(Invitation::class);
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(Colocation::class);
+    }
+
+    public function expense()
+    {
+        return $this->hasMany(Expense::class);
+    }
+
+    public function settlement()
+    {
+        return $this->hasMany(Settlement::class);
+    }
 }
